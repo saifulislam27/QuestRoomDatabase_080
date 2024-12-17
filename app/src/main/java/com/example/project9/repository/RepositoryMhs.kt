fa09efcd2068409884b3626d239aa9eeacb1bbf4
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-17 11:10:25 +0700

    menambah object dan route untuk masing masing navigasi "home", "detail", "update"

--- app/src/main/java/com/example/project9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/project9/repository/RepositoryMhs.kt
@@ -1,8 +1,17 @@
 package com.example.project9.repository
 
 import com.example.project9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
 
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs () : Flow<List<Mahasiswa>>
+
+    fun  getMhs (nim: String) : Flow<Mahasiswa>
+
+    suspend fun deleteMhs (mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs (mahasiswa: Mahasiswa)
 }
--- app/src/main/java/com/example/project9/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/project9/ui/navigation/AlamatNavigasi.kt
@@ -3,4 +3,20 @@ package com.example.project9.ui.navigation
 interface AlamatNavigasi {
 
     val route : String
+
+    object DestinasiHome : AlamatNavigasi {
+        override val route = "home"
+    }
+
+    object DestinasiDetail : AlamatNavigasi {
+        override val route = "detail"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM}"
+    }
+
+    object DestinasiUpdate : AlamatNavigasi{
+        override val route = "update"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM}"
+    }
 }
