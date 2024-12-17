e6937df04a11e11ed5824bcdf78a4c477ccbb02a
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-17 11:42:05 +0700

    membuat val isUiEventEmpty dan function toDetailUiEvent

--- app/src/main/java/com/example/project9/ui/viewmodel/DetailMhsModel.kt
+++ app/src/main/java/com/example/project9/ui/viewmodel/DetailMhsModel.kt
@@ -1,5 +1,7 @@
 package com.example.project9.ui.viewmodel
 
+import com.example.project9.data.entity.Mahasiswa
+
 class DetailMhsModel (
 )
 data class DetailMhsUiState(
@@ -7,4 +9,19 @@ data class DetailMhsUiState(
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
-)
+){
+    val isUiEventEmpty: Boolean
+        get()= detailUiEvent == MahasiswaEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
