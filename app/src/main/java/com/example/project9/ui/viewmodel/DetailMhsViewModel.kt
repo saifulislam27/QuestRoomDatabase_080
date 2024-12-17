4ed15f83244be58d06c15c70b85fc8097bb76b7a
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-17 11:48:14 +0700

    Menambahkan function deleteMhs

--- app/src/main/java/com/example/project9/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/project9/ui/viewmodel/DetailMhsViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -50,6 +51,13 @@ class DetailMhsViewModel (
                 isLoading = true
             ),
         )
+    fun deleteMhs() {
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
 }
 data class DetailMhsUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
