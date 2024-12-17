c08ac436faf053b797faa416b37a3fa3bd69b356
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-17 13:22:59 +0700

    membuat @COMPOSABLE dengan private function DeleteConfirmationDialog

---
+++ app/src/main/java/com/example/project9/ui/view/mahasiswa/DetailMhsView.kt
@@ -0,0 +1,31 @@
+package com.example.project9.ui.view.mahasiswa
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah Anda Yakin Ingin Menghapus Data Ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
